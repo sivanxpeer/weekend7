9cde1e04272522e19692e06e2066605afafa8b1e
sivanxpeer <sivan.x.peer@gmail.com>
2021-12-11 23:10:44 +0200

    getting regions into same object

--- index.js
+++ index.js
@@ -6,8 +6,67 @@ const proxy = "https://intense-mesa-62220.herokuapp.com/";
 
 const countriesCodes = [];
 const countriesNames = [];
-const countriesData = [];
-// const regions = [];
+// const countriesData = [];
+
+async function getCountryData() {
+    let countriesData = [];
+    let response = await fetch('https://corona-api.com/countries');
+    // let response = await fetch(`${proxy}${baseStatsURL}`);
+    let responseJ = await response.json();
+    // console.log(responseJ);
+    for (let rowItem of responseJ.data) {
+        // console.log(rowItem);
+        countriesData.push({
+            "name": rowItem.name,
+            "code": rowItem.code,
+            "confirmed cases": rowItem.latest_data.confirmed,
+            "number of deaths": rowItem.latest_data.deaths,
+            "number of recovered": rowItem.latest_data.deaths,
+            "number of critical conditions": rowItem.latest_data.critical,
+        })
+    }
+    return countriesData;
+}
+
+async function getRegions(countriesData) {
+    const data = await fetch(`${proxy}${baseCountriesURL}`);
+    const dataJ = await data.json();
+    // console.log(dataJ)
+    for(let i=0;i<countriesData.length;i++){
+        if(dataJ[i].name.common===countriesData[i].name){
+            countriesData[i].region = dataJ[i].region;
+        }
+    }
+    return countriesData;
+}
+
+// async function getContriesCode() {
+//     let data = await fetch(`${proxy}${baseCountriesURL}`);
+//     let dataJ = await data.json();
+//     console.log(dataJ);
+//     for (let rowItem of dataJ) {
+//         countriesCodes.push(rowItem.cca2);
+//     }
+// }
+
+
+async function main() {
+    // console.log(worldArrayOfObjects);
+    let countriesData = await getCountryData();
+    // console.log(countriesData);
+    countriesData = await getRegions(countriesData);
+    console.log(countriesData);
+    // getRegions();
+
+}
+
+main();
+
+
+
+
+
+
 
 {/* <canvas id="myChart" width="400" height="400"></canvas>; */ }
 const ctx = document.getElementById("myChart").getContext("2d");
@@ -47,61 +106,3 @@ const myChart = new Chart(ctx, {
         },
     },
 });
-
-
-async function getContriesCode() {
-    let data = await fetch(`${proxy}${baseCountriesURL}`);
-    let dataJ = await data.json();
-    console.log(dataJ);
-    for (let rowItem of dataJ) {
-        countriesCodes.push(rowItem.cca2);
-    }
-}
-
-async function getRegionsByCountryCode(){
-    
-}
-
-// const regions = [];
-// async function getRegion() {
-//     let data = await fetch(`${proxy}${baseCountriesURL}`);
-//     let dataJ = await data.json();
-//     // console.log(dataJ);
-
-//     for (let rowItem of dataJ) {
-//         // setTimeout(()=>{
-//         return (rowItem.region);
-
-//         //         regions.push({
-//         //             "region": rowItem.region,
-//         //         })
-//         //         // console.log(regions);
-//         // }
-//         // return regions;
-//     }
-// }
-
-async function getCountryData() {
-    let response = await fetch('https://corona-api.com/countries');
-    let responseJ = await response.json();
-    // console.log(responseJ);
-    for (let rowItem of responseJ.data) {
-        // console.log(rowItem);
-        countriesData.push({
-            "name": rowItem.name,
-            "code": rowItem.code,
-            "confirmed cases": rowItem.latest_data.confirmed,
-            "number of deaths": rowItem.latest_data.deaths,
-            "number of recovered": rowItem.latest_data.deaths,
-            "number of critical conditions": rowItem.latest_data.critical,
-
-        })
-    }
-}
-
-
-getCountryData();
-console.log(countriesData);
-
-// console.log(regions);
-// getRegions();
